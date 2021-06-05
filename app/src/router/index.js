81b8faa6bbde463a892a1d52458c418ddcb60ab0
dahyekim <rlaekgp94@naver.com>
2021-06-05 13:36:16 +0900

    merge충돌방지 api페이지만 연결

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TheMain from "../views/TheMain.vue";
 import TheLogin from "../views/TheLogin.vue";
-import TheEmailLogin from "../views/TheEmailLogin.vue";
 import TheApiLogin from "../views/TheApiLogin.vue";
 
 const routes = [
@@ -14,12 +13,6 @@ const routes = [
     path: "/login",
     name: "login",
     component: TheLogin,
-    meta: { hideHeader: true, hideFooter: true },
-  },
-  {
-    path: "/login/email-login",
-    name: "email-login",
-    component: TheEmailLogin,
     meta: { hideHeader: true, hideFooter: true },
   },
   {
